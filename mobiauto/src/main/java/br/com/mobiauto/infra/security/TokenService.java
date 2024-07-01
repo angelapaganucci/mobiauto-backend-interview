6e0012f2d3b04391531808eba3103302667d89fb
Angela Paganucci <angela.luiz@db1.com.br>
2024-07-01 17:31:03 -0300

    Correção

--- mobiauto/src/main/java/br/com/mobiauto/infra/security/TokenService.java
+++ mobiauto/src/main/java/br/com/mobiauto/infra/security/TokenService.java
@@ -23,7 +23,7 @@ public class TokenService {
             Algorithm algorithm = Algorithm.HMAC256(secret);
 
             String token = JWT.create()
-                    .withIssuer("login-auth-api")
+                    .withIssuer("mobiauto")
                     .withSubject(usuario.getEmail())
                     .withExpiresAt(this.generateExpirationDate())
                     .sign(algorithm);
@@ -37,7 +37,7 @@ public class TokenService {
         try {
             Algorithm algorithm = Algorithm.HMAC256(secret);
             return JWT.require(algorithm)
-                    .withIssuer("login-auth-api")
+                    .withIssuer("mobiauto")
                     .build()
                     .verify(token)
                     .getSubject();
